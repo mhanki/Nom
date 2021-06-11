0ad86d5483daa1445a08520a0087e5ba652cfeec
mhanki <meike.hankewicz@protonmail.com>
2021-06-11 19:37:42 +0200

    add method to clear console without losing history

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
+import readline from 'readline'
 import print from './print.js'
+
+// used instead of console.clear() to preserver history
+const _clearConsole = () => {
+  const blank = '\n'.repeat(process.stdout.rows)
+  console.log(blank)
+  readline.cursorTo(process.stdout, 0, 0)
+  readline.clearScreenDown(process.stdout)
+}
 
 const nom = () => {
   let frame = 0
@@ -10,7 +19,7 @@ const nom = () => {
     }
 
     frame += 1
-    console.clear()
+    _clearConsole()
 
     print(frame)
   }
